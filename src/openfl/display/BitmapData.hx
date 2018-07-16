58a050fe3661d581a4a9f4cb551fb2b0ce7cdfd6
Dan Korostelev <nadako@gmail.com>
2018-07-16 11:19:13 +0200

    remove unused leftover __updateMask method
    (cherry picked from commit 5777c5f)

--- src/openfl/display/BitmapData.hx
+++ src/openfl/display/BitmapData.hx
@@ -2076,13 +2076,6 @@ class BitmapData implements IBitmapDrawable {
 	}
 	
 	
-	private function __updateMask (maskGraphics:Graphics):Void {
-		
-		
-		
-	}
-	
-	
 	private function __updateTransforms (overrideTransform:Matrix = null):Void {
 		
 		if (overrideTransform == null) {
